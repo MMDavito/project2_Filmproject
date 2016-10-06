f2a4ce9b1b391333bd79b86aeb3b06d33444be55
MMDavito <davidking9753@gmail.com>
2016-10-06 14:30:48 +0200

    FirstCommit

--- src/project2filmproject/Project2Filmproject.java
+++ src/project2filmproject/Project2Filmproject.java
@@ -6,6 +6,9 @@
 package project2filmproject;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.util.Calendar;
 
 /**
  *
@@ -22,8 +25,19 @@ public class Project2Filmproject {
             String password = "";
             Connection connection = ConnectionFactory.getConnection(userName, password);
             //lägg till Hunt for the Wilderpeople till databasen, testa sen skriv ut (lägg bara till med ren kod)
+            Calendar calandar = Calendar.getInstance();
+            java.sql.Date startDate = new java.sql.Date(calandar.getTime().getTime());
+
+            String query = " insert into filmregister (filmnamn, regissör, redigerad_datum)" + "values (?, ?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, "Hunt for the Wilderpeople");
+            preparedStatement.setString(2, "Taika Waititi");
+            preparedStatement.setDate(3, startDate);
+            preparedStatement.execute();
+            connection.close();
         } catch (Exception e) {
+            System.out.println("Error" + e);
         }
     }
-
 }
+
